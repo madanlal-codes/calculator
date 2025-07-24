2183a29c03796522d0e83242871e2688f7b7de4f
madanlal.codes <madanlal85050@gmail.com.com>
2025-07-24 23:16:41 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const display = document.getElementById("display");
+
+function appendNumber(num) {
+  if (display.innerText === "0") {
+    display.innerText = num;
+  } else {
+    display.innerText += num;
+  }
+}
+
+function appendOperator(op) {
+  const lastChar = display.innerText.slice(-1);
+  if ("+-*/%".includes(lastChar)) return;
+  display.innerText += op;
+}
+
+function clearDisplay() {
+  display.innerText = "0";
+}
+
+function deleteLast() {
+  if (display.innerText.length === 1) {
+    display.innerText = "0";
+  } else {
+    display.innerText = display.innerText.slice(0, -1);
+  }
+}
+
+function calculate() {
+  try {
+    let result = eval(display.innerText);
+    display.innerText = result;
+  } catch {
+    display.innerText = "Error";
+  }
+}
